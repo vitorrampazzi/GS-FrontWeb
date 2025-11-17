1b03305c47928c8e6e14118719134be619836c38
vitorrampazzi <vitor.rampazzi@gmail.com>
2025-11-17 15:09:55 -0300

    Tentando arrumar Darkmode

--- gs_frontdev/src/components/ProfileList.js
+++ gs_frontdev/src/components/ProfileList.js
@@ -26,7 +26,7 @@ export default function ProfileList({ allProfiles }) {
     if (isDark) {
       document.documentElement.classList.add('dark');
     } else {
-      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
     }
   }, [isDark]); 
 
--- gs_frontdev/tailwind.config.mjs
+++ gs_frontdev/tailwind.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  // ESTA LINHA É A MAIS IMPORTANTE DE TODAS:
   darkMode: 'class', 
-
+  
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 };
+
 export default config;
